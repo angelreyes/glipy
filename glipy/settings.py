cfb2f3e73747fc2fd03345907d7909c52c5a4fff
Angel G. Reyes <angelgabriel.reyes@yahoo.com>
2013-10-09 09:42:04 +0600

    :) passwd cannot br public

--- glipy/settings.py
+++ glipy/settings.py
@@ -13,8 +13,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'glipy',                      # Or path to database file if using sqlite3.
-        'USER': '*',
-        'PASSWORD': '*',
+        'USER': 'wamdbadmin',
+        'PASSWORD': '741WAMDBAdmin',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
